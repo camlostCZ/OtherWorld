c49cd7fb70700f4bb1cf687efe47181bd83c29ba
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2023-07-06 14:17:01 +0000

    Refactor inventory - add methods to add, get and remove items

--- otherworld/inventory.py
+++ otherworld/inventory.py
@@ -1,8 +1,13 @@
 from item import OtherWorldItem
 
+class InventoryError(RuntimeError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
 class InventoryItem:
-    def __init__(self):
-        self.id: str
+    def __init__(self, id):
+        self.id: str = id
         self.item: OtherWorldItem
         self.count: int = 1
 
@@ -12,28 +17,91 @@ class InventoryItem:
 
 class OtherWorldInventory:
     """
-    Keeps an inventory of items. 
+    Keeps an inventory of items.
     
     Used as player's inventory, player's equipment list,
     and list of items on a map.
+
+    The game should use character codes ('a' .. 'z') to ease the selection
+    of items from inventory. These codes are directly translatable to index
+    - a -> 0, b -> 1, etc.
     """
+    CODE_SET = [chr(ord('a') + x) for x in range(26)]
 
-    def __init__(self, max_items: int) -> None:
+    def __init__(self, name: str, max_items: int) -> None:
+        self.name = name
         self.max_items = max_items
-        self.items: dict[str, InventoryItem] = {}
+        self.items: list[InventoryItem] = []
 
 
-    def add_item(self, item_name: str) -> None:
-        # TODO Add an item to the inventory
-        # i.e. assign a key and add it to the dict.
-        pass
+    def add_item(self, item_id: str) -> None:
+        """
+        Add an item into the inventory.
+
+        Args:
+            item_id (str): Item ID
+
+        Raises:
+            InventoryError: In case of full inventory.
+        """
+        idx = self.get_item_idx(item_id)
+        if idx >= 0:    # Item found
+            self.items[idx].count += 1
+        elif len(self.items) < self.max_items:
+            self.items.append(InventoryItem(item_id))
+        else:
+            raise InventoryError("Invetory full")
 
 
-    def get_item(self, id: str) -> InventoryItem:
-        # TODO Return an InventoryItem object from the inventory
-        pass
+    def get_item_idx(self, item_id: str) -> int:
+        """
+        Search for item index in the inventory.
+
+        Args:
+            item_id (str): Item ID
+
+        Returns:
+            int: Index of the item in the inventory, or -1 if not found.
+        """
+        result = -1
+        for idx, each in enumerate(self.items):
+            if each.id == item_id:
+                result = idx
+                break
+        return result
 
 
-    def remove_item(self, id: str) -> None:
-        # TODO Remove an item from inventory
-        pass
+    def remove_item(self, item_id: str) -> None:
+        """
+        Remove an item from the inventory.
+
+        Args:
+            item_id (str): Item ID
+
+        Raises:
+            InventoryError: If the item is not present in the inventory.
+        """
+        idx = self.get_item_idx(item_id)
+        if idx >= 0:    # Item found
+            item = self.items[idx]
+            item.count += -1
+            if item.count == 0: # Last piece of an item removed
+                self.items = self.items[:idx] + self.items[idx:]       
+        else:
+            raise InventoryError("Item not found")
+        
+
+    def get_item_idx_by_code(self, code: str) -> int:
+        """
+        Get item by so-called code which is typically a single letter.
+        Can be overridden in child classes to increase the maximum capacity
+        on inventories.
+
+        Args:
+            code (str): An item code
+
+        Returns:
+            int: Index translated from the code
+        """
+        idx = OtherWorldInventory.CODE_SET.index(code)
+        return idx
