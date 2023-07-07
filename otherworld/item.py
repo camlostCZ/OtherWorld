8d5e64c047c5c093b39df32691efeea9ed9041f4
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2023-07-07 23:11:44 +0000

    Refactor loading item from YAML file

--- otherworld/item.py
+++ otherworld/item.py
@@ -4,30 +4,19 @@ from yaml import safe_load
 from baseclasses import YAMLSourced
 
 
+class ItemError(ValueError):
+    pass
+
+
 class OtherWorldItem(YAMLSourced):
-    def __init__(self) -> None:
+    def __init__(self, id: str, name: str, title: str, description: str,
+        weight: float) -> None:
         super().__init__()
-        self.id: str
-        self.name: str
-        self.title: str
-        self.description: str
-        self.weight: float
-        self.flags: list[str]
-        self.effects: dict[str, Any]
-        
+        self.id: str = id
+        self.name: str = name
+        self.title: str = title
+        self.description: str = description
+        self.weight: float = weight
+        self.flags: list[str] = []
+        self.effects: dict[str, Any] = {}
 
-    def load_yaml_file(self, fd: TextIO) -> None:
-        """
-        Load a map from a YAML file
-
-        Args:
-            fd (TextIO): File descriptor of the source YAML file
-        """
-        data = safe_load(fd)
-        self.id = data["id"]
-        self.name = data["name"]
-        self.title = data["title"]
-        self.description = data["description"]
-        self.weight = data["weight"]
-        self.flags = data["flags"]
-        self.effects = data["effects"]
