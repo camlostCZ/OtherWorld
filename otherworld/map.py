b173f33893bb1483376b212ded4300811f393cb4
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2023-07-07 23:11:32 +0000

    Refactor loading map from YAML file

--- otherworld/map.py
+++ otherworld/map.py
@@ -12,42 +12,10 @@ class OtherWorldMap(YAMLSourced):
     A map - class representing a single map in a world.
     """
 
-    def __init__(self) -> None:
-        self.id = ""
-        self.title = ""
-        self.description = ""
+    def __init__(self, id: str, title: str, description: str) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
         self.exits = {}
         self.inventory = OtherWorldInventory("map_items")
         self.effects: list[Effect] = []
-
-
-    def load_yaml_file(self, fd: TextIO) -> None:
-        """
-        Load a map from a YAML file
-
-        Args:
-            fd (TextIO): File descriptor of the source YAML file
-        """
-        data = safe_load(fd)
-        self.id = data["id"]
-        self.title = data["title"]
-        self.description = data["description"]
-
-        if "exits" in data:
-            for item in data["exits"]:
-                for key, val in item.items():
-                    self.exits[key] = val
-
-        if "items" in data:
-            for item in data["items"]:
-                id, count = item
-                self.inventory.add_item(id, count)
-
-        if "effects" in data:
-            for each in data["effects"]:
-                try:
-                    eff = Effect(each["name"], each["stat"], each["effect"], each["duration"])
-                    self.effects.append(eff)
-                except KeyError:
-                    # Skip invalid records
-                    pass
